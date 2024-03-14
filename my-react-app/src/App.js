37bd2e53aafa94b3fc253f12190caa4e7943edba
anwar-opu <anwar9437@gmail.com>
2024-03-15 00:37:45 +0600

    routing issue

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+// import Navbar from '../Components/Navbar/Navbar';
+
+// import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Shop />,
+    },
+    {
+      path: '/mens',
+      element: <div>Mens</div>,
+    },
+    {
+      path: '/womens',
+      element: <div>womans</div>,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+/*<BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="mens" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>*/
