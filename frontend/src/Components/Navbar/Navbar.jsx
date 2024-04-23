1cb8c95ff0c5b5d7fbe0b84a9a96a78068aae233
anwar-opu <anwar9437@gmail.com>
2024-04-23 15:50:44 +0600

    offers banner page  responsive complete

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,31 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import './../Navbar/Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
-
+import nav_dropdown from '../Assets/nav_dropdown.png';
 const Navbar = () => {
   const { getTotalCartitem } = useContext(ShopContext);
   const [menu, setMenu] = useState('shop');
+  const menuRef = useRef();
+
+  const dropdown_toggle = e => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  };
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+
+      <div className="nav-dropdown">
+        <img onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      </div>
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu('shop');
